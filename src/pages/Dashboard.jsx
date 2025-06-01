db91fde1077fcb2b63881572e48737c5f072c158
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-01 17:03:37 +0700

    Remake with react vite tailwindcss init

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <section className="w-full h-full">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-9xl">Dashboard</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Dashboard;
