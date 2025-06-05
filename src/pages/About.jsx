baab7f38953bd592d32144f4c8b6c275a822b622
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-05 17:10:08 +0700

    A little about my programming journey

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 const About = () => {
   return (
-    <section id="about" className="flex flex-col justify-center px-10 min-h-screen bg-white">
-      <h2 className="text-4xl">About Me</h2>
-      <p>Lorem ipsum dolor sit amet 
-        consectetur adipiscing elit. Dolor sit amet
-        consectetur adipiscing elit quisque faucibus.
+    <section id="about" className="md:w-1/2 flex flex-col justify-center px-10 gap-3 min-h-screen bg-white">
+      <h1 className="text-3xl md:text-4xl text-balanced">
+        Andi Fajar Ramadhan
+      </h1>
+      <p className="text-base md:text-lg leading-relaxed">
+        Started my programming journey by join a coding scolarship in 2024 with focus 
+        on BackEnd & FrontEnd Development with JavaScript.
       </p>
     </section>
   )
