1312817d6a27e3515f152ffbd02e0abd46cf5e83
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-09 17:34:37 +0700

    fixed typo

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -2,7 +2,7 @@ const Maintenance = () => {
   return (
     <section className="flex flex-col justify-center items-center text-pretty text-center text-my-black gap-2">
       <h1 className="text-5xl md:text-7xl text-my-cyan">Sorry!</h1>
-      <h2 className="text-2xl md:text-4xl text-my-red">This Page is still in maintenance</h2>
+      <h2 className="text-2xl md:text-4xl text-my-red">This page is still in maintenance</h2>
       <p className=" md:text-xl">Stay tune for the pages information on my LinkedIn!</p>
     </section>
   );
