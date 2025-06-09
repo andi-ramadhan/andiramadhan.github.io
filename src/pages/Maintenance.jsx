98997148737e3d6540daa1a9df0f09b4d09d863d
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-09 17:28:04 +0700

    Add Maintenance page

---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,11 @@
+const Maintenance = () => {
+  return (
+    <section className="flex flex-col justify-center items-center text-pretty text-center text-my-black gap-2">
+      <h1 className="text-5xl md:text-7xl text-my-cyan">Sorry!</h1>
+      <h2 className="text-2xl md:text-4xl text-my-red">This Page is still in maintenance</h2>
+      <p className=" md:text-xl">Stay tune for the pages information on my LinkedIn!</p>
+    </section>
+  );
+}
+
+export default Maintenance;
