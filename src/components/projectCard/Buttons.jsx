2f9e1b4a7d39e395ec6550d5b3246fc9cf4ce7a2
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-12 14:06:37 +0700

    Cleaning and make ViewButton more modular

--- src/components/projectCard/Buttons.jsx
+++ src/components/projectCard/Buttons.jsx
@@ -1,18 +1,13 @@
-import { Link, NavLink } from "react-router";
+import { Link } from "react-router";
+import ViewButton from "../ViewButton";
 
 const Buttons = ({ btnLWidth = '', btnRWidth = '', viewTo = '', liveTo = '' }) => {
   return (
     <>
-      <NavLink 
-        to={'/project'} 
-        className={`${btnLWidth} h-10 
-        flex gap-2 justify-center items-center ring-2 ring-my-cyan font-semibold tracking-wide rounded-lg`}
-      >
-        <p className="text-my-cyan">View Project</p>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className="size-3 text-my-cyan">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-        </svg>
-      </NavLink>
+      <ViewButton 
+        btnWidth={btnLWidth}
+        viewTo={viewTo}
+      />
       <Link
         to={liveTo}
         target="_blank"
