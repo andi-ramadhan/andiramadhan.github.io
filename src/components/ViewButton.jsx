0529191de508cfda6f372b176aedfedc442088ad
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-12 14:07:04 +0700

    View Project button

---
+++ src/components/ViewButton.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router";
+
+const ViewButton = ({ btnWidth = '', viewTo = '', btnHeight = 'h-10' }) => {
+  return (
+    <NavLink 
+      to={viewTo} 
+      className={`${btnWidth} ${btnHeight} 
+      flex gap-2 justify-center items-center ring-2 ring-my-cyan font-semibold tracking-wide rounded-lg`}
+    >
+      <p className="text-my-cyan">View Project</p>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor" className="size-3 text-my-cyan">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+      </svg>
+    </NavLink>
+  );
+}
+
+export default ViewButton;
