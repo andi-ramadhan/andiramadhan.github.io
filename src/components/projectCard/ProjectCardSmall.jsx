3d43e7cc5765a07539cae7892c71916d68fa362f
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-12 16:01:46 +0700

    make it more precision with grid at the main container and flex 1 for the desc

--- src/components/projectCard/ProjectCardSmall.jsx
+++ src/components/projectCard/ProjectCardSmall.jsx
@@ -1,9 +1,9 @@
 import Buttons from "./Buttons";
 import TagCard from "../TagCard";
 
-const ProjectCardSmall = ({ imgSrc = '', title = '', desc = '', tags = [], btnLWidth = 'w-3/5', btnRWidth = 'w-2/5', viewLink = '#', liveLink = '#' }) => {
+const ProjectCardSmall = ({ imgSrc = '', title = '', desc = '', tags = [], btnLWidth = 'w-3/5', btnRWidth = 'w-2/5', viewLink = '#', liveLink = '#', repoLink = '' }) => {
   return (
-    <div className="w-full md:h-[600px] flex flex-col justify-between gap-5 p-6 rounded-lg shadow-lg shadow-my-black/20">
+    <div className="w-full md:h-[600px] grid grid-rows-[auto_1fr_auto_auto] gap-5 p-6 rounded-lg shadow-lg shadow-my-black/20">
       
       {/* IMAGE */}
       <figure className="w-full h-36 md:h-50 overflow-hidden rounded-md">
@@ -18,12 +18,12 @@ const ProjectCardSmall = ({ imgSrc = '', title = '', desc = '', tags = [], btnLW
       {/* CONTENT */}
       <div className="flex flex-col justify-center gap-3">
         <h2 className="text-2xl md:text-3xl font-semibold text-balance text-my-red">{title}</h2>
-        <div className="flex justify-center">
-          <p className="md:text-lg text-my-black flex-1">
+        <div className="flex-1 justify-center">
+          <p className=" text-my-black flex-1 py-1">
             {desc}
           </p>
         </div>
-        <div className="flex-wrap flex gap-2 items-center text-xs md:text-base text-my-black">
+        <div className="flex flex-wrap gap-2 items-center text-xs md:text-base text-my-black">
           <p className="text-my-black font-semibold">Tools:</p>
           {tags.map((tag, idx) => (
             <TagCard
@@ -34,6 +34,9 @@ const ProjectCardSmall = ({ imgSrc = '', title = '', desc = '', tags = [], btnLW
           ))}
         </div>
       </div>
+      <p className="flex items-center gap-2">
+        <i className="fa-brands fa-github text-xl md:text-2xl text-my-black"></i> GitHub: {repoLink ? <a href={repoLink} target="_blank" className="font-medium text-my-cyan">Link</a> : '[Private]'}
+      </p>
 
       {/* BUTTON */}
       <div className="flex justify-between gap-3 text-sm py-3">
