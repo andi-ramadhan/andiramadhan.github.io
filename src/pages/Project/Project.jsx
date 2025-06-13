5a618cd7e6f5c2f5d7ab843d6cca2ce91403d32a
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-13 17:43:11 +0700

    Refactored, modularized, centralized

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,20 +1,9 @@
-import { useEffect, useRef, useState } from "react";
 import ProjectCardSmall from "../../components/projectCard/ProjectCardSmall";
-import MaintenanceModal from "./ProjectView/MaintenanceModal";
 import { motion } from "framer-motion";
 import Footer from "../../components/Footer";
+import { ProjectData } from "./ProjectView/ProjectDetail.data";
 
 const Project = () => {
-  const [showModal, setShowModal] = useState(false);
- 
-  const handleContainerClick = (e) => {
-    const link = e.target.closest('a[href="#maintenance"]');
-    if (link) {
-      e.preventDefault();
-      setShowModal(true);
-    }
-  };
-
   return (
     <section id="project" className="flex flex-col md:items-center w-[85%] md:w-[80%] py-25 md:py-40 gap-10 text-my-black">
       <motion.h1 
@@ -26,85 +15,31 @@ const Project = () => {
       >
         My Works
       </motion.h1>
-      <div 
-        onClick={handleContainerClick}
-        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
-      >
-        <motion.div
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {ProjectData.map((project, idx) => (
+          <motion.div
+          key={project.key}
           initial= {{ opacity: 0, y: 20 }}
           whileInView= {{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4, delay: 0.5 }}
+          transition={{ duration: 0.4, delay: 0.5 + idx * 0.1 }}
           viewport={{ once: true }}
-        >
-          <ProjectCardSmall
-            id="siichiseivocalcourse"
-            imgSrc="/assets/siichisei.webp"
-            title="SiiChiSei Vocal Course"
-            desc="A Company Profile and Vocal Course Information website. Purposed to serve a vocal courses information, prioritized to give
-                  a clarity of the services to user."
-            tags={[
-              { child: 'React Router' }, { child: 'Vite' }, { child: 'TailwindCSS' }, { child: 'EmailJS' }
-            ]}
-            viewLink="#maintenance"
-            liveLink='https://siichisei-vocalcourse.pages.dev/'
-          />
-        </motion.div>
-
-        <motion.div
-          initial= {{ opacity: 0, y: 20 }}
-          whileInView= {{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4, delay: 0.6 }}
-          viewport={{ once: true }}
-        >
-          <ProjectCardSmall
-            imgSrc="/assets/webcafe.webp"
-            title="Web Cafe Menu (OnProgress)"
-            desc="Full-Stack Web Project, Cafe menu web based
-                that serve a transparancy between customer and the menu order cycle."
-            tags={[
-              { child: 'React' }, { child: 'Vite' }, { child: 'TailwindCSS' }, { child: 'ExpressJs' },
-              { child: 'NodeJs' }, { child: 'MongoDB' }
-            ]}
-            btnLWidth="w-full"
-            btnRWidth="hidden"
-            viewLink="#maintenance"
-            repoLink="https://github.com/andi-ramadhan/web-cafe-menu"
-          />
-        </motion.div>
-
-        <motion.div
-          initial= {{ opacity: 0, y: 20 }}
-          whileInView= {{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4, delay: 0.7 }}
-          viewport={{ once: true }}
-        >
-          <ProjectCardSmall
-            imgSrc="https://opengraph.githubassets.com/1/andi-ramadhan/openmusic-api"
-            title="OpenMusic API"
-            desc="A Restful API Music Library, provides security in creating a music library.
-            User also can collaborate to make their music library together"
-            tags={[
-              { child: 'HapiJs' }, { child: 'PostgreSQL' }, { child: 'Redis' }, { child: 'RabbitMQ' },
-              { child: 'JWT' }, { child: 'Joi' }, { child: 'Bcrypt' }, { child: 'NodeJs' }
-            ]}
-            btnLWidth="w-full"
-            btnRWidth="hidden"
-            viewLink="#maintenance"
-            repoLink="https://github.com/andi-ramadhan/openmusic-api"
-          />
-        </motion.div>
-      </div>
-      {showModal && (
-        <div className="fixed inset-0 z-50 flex justify-center items-center bg-my-black/50">
-          <MaintenanceModal />
-          <button
-            className="absolute top-5 right-5 text-my-red text-3xl"
-            onClick={() => setShowModal(false)}
           >
-            &times;
-          </button>
-        </div>
-      )}
+            <ProjectCardSmall 
+              id={project.key}
+              imgSrc={project.imgSrc}
+              title={project.title}
+              desc={project.briefDesc}
+              tags={project.tags}
+              viewLink={`/project/${project.slug}`}
+              liveLink={project.link}
+              repoLink={project.github}
+              btnLWidth={ project.link === '' ? "w-full" : 'w-3/5' }
+              btnRWidth={ project.link === '' ? "hidden" : 'w-2/5' }
+            />    
+          </motion.div>
+        ))}
+
+      </div>
       <Footer />
     </section>
   )
