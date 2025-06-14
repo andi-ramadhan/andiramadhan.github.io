b455ce63477c1fb9e9b5f269b56884ca57f61f7a
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-14 13:20:43 +0700

    re-adjust padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center pt-20 text-center text-sm opacity-50 text-my-black tracking-wide">
+    <div className="w-full flex flex-col items-center justify-center text-center text-sm opacity-50 text-my-black tracking-wide">
       <p>&copy; 2025 Andi Fajar Ramadhan</p>
       <p>All rights reserved</p>
     </div>  
