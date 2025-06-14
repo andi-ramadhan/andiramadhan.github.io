73dff602d0ba7539898925e6800faacbe9e6b132
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-14 16:52:36 +0700

    Add motion and fixing the design of ProjectDetail, also filling the projectdetails data

--- src/pages/Project/ProjectView/ProjectDetail.data.js
+++ src/pages/Project/ProjectView/ProjectDetail.data.js
@@ -13,18 +13,18 @@ export const ProjectData = [
                 that can attract a prospective students of the subject. This website was made for
                 my wife indie company that runs in vocal and singing things.`,
     tags      : [
-                  { child:'React Router' },
-                  { child:'Vite' },
+                  { child: 'React Router' },
+                  { child: 'Vite' },
                   { child: 'TailwindCSS' },
                   { child: 'EmailJS' },
                   { child: 'Motion' }
                 ],
     features  : [
-                  "Responsive Design",
-                  "Lazy Loading",
-                  "Modular File Structure",
-                  "SEO Basic (Upgradable)",
-                  "Deployment on Cloudflare Pages"
+                  "💻 Responsive Design",
+                  "🔎 Lazy Loading",
+                  "📁 Modular File Structure",
+                  "🌐 SEO Basic (Upgradable)",
+                  "☁️ Deployment on Cloudflare Pages"
                 ],
     challenges: `Handling responsive layout was so tricky, especially this is my first time
                 made a website from scratch so first I make the components modular, reusable
@@ -40,7 +40,10 @@ export const ProjectData = [
     github    : 'https://github.com/andi-ramadhan/web-cafe-menu',
     briefDesc : `Full-Stack Web Project, Cafe menu web based
                 that serve a transparancy between customer and the menu order cycle.`,
-    desc      : `Not yet`,
+    desc      : `This project is planned for measure my knowledge and skills in both side
+                (Backend and Frontend). This Cafe web based menu purposed to serve customer
+                with a seamlessly menu website with a comforting UI, provide a transparancy 
+                between the order cycle.`,
     tags      : [
                   { child: 'React' },
                   { child: 'Vite' }, 
@@ -50,9 +53,18 @@ export const ProjectData = [
                   { child: 'MongoDB' }
                 ],
     features  : [
-                  "Not done yet"
+                  "- These features below are the planned features -",
+                  "💻 Responsive Design (prioritizing Mobile and Tablet)",
+                  "🔎 Lazy Loading",
+                  "🛒 Order Cart with Calculation",
+                  "💳 Payment method option (Cash or Bank Transfer)",
+                  "⏱️ Food preparation time monitor and estimation",
+                  "☁️ Deployment on Cloudflare Worker & Pages"
                 ],
-    challenges: `Not done yet` 
+    challenges: `It's really a big challenge that I must faced to measure my knowledge in Full-Stack Engineering.
+                Choosing the architecture and file structure also a crucial thing that I must consider it carefully.
+                So far it's work just to fetch the image from cloud and show it to end-user. I'll rewrite this text 
+                after the major update of this project` 
   },
   {
     key       : 'OMA',
@@ -63,7 +75,10 @@ export const ProjectData = [
     github    : 'https://github.com/andi-ramadhan/openmusic-api',
     briefDesc : `A Restful API Music Library, provides security in creating a music library.
                 User also can collaborate to make their music library together`,
-    desc      : `Not yet`,
+    desc      : `OpenMusic API is a RESTful back-end service designed for managing music-related data, 
+                including albums, songs, users, playlists, and social features. Built with Hapi.js, 
+                it offers a modular architecture with robust features like JWT authentication, 
+                Redis caching, and RabbitMQ-based asynchronous processing.`,
     tags      : [
                   { child: 'HapiJs' }, 
                   { child: 'PostgreSQL' }, 
@@ -75,8 +90,22 @@ export const ProjectData = [
                   { child: 'NodeJs' }
                 ],
     features  : [
-                  "Not done yet"
+                  "➕ CRUD Services",
+                  "🔎 Authorization & Authentication",
+                  "📁 User, Album, Song, and Playlist Management",
+                  "🌐 Users Collaboration",
+                  "📈 Activity Tracking",
+                  "🖼️ Album Cover",
+                  "👍 Likes System",
+                  "📤 Export Playlist"
                 ],
-    challenges: `Not done yet` 
+    challenges: `It's been a challenging and thrilling exprience since this
+                project is also a submission project for my BackEnd Intermediate
+                scholarship. The most challenging things is when I need to integrate
+                the Message Broker (RabbitMQ) with the initial service (CRUD), then
+                I started to realize the architecture and then implement Consumer and
+                Producer method, seperated the Message Broker Consumer and Producer so
+                it can be delivered between those services when user going to export the
+                playlist.`
   },
 ]
--- src/pages/Project/ProjectView/ProjectDetail.jsx
+++ src/pages/Project/ProjectView/ProjectDetail.jsx
@@ -1,6 +1,8 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import TagCard from "../../../components/TagCard";
 import { ProjectData } from "./ProjectDetail.data";
+import { LinkIcon, UnlinkIcon } from "../../../components/icons/linkIcons";
+import { motion } from "framer-motion";
 
 const ProjectDetail = () => {
   const { slug } = useParams();
@@ -9,53 +11,141 @@ const ProjectDetail = () => {
   if (!project) return <div>Project not found</div>
 
   return (
-    <section className="bg-my-white flex flex-col items-center py-30 gap-20 text-my-black">
-      <div className="px-4 flex flex-col gap-5 w-[85%] md:w-[50%]">
-        <h1 className="text-3xl md:text-4xl text-my-cyan border-my-cyan border-l-5 pl-3">{project.title}</h1>
-        <img src="/assets/siichisei.webp" />
-        <div className="flex justify-between py-1 border-my-black/40 border-y italic text-sm md:text-base">
-          <p>
-            GitHub: {project.github ? <a href={project.github} target="_blank" className="font-semibold text-my-cyan/70">Link</a> : '[Private]'}
-          </p>
-          <Link to={project.link} target="_blank" className="font-semibold text-my-cyan/70">Open Live Site</Link>
-        </div>
+    <section className="bg-my-white flex flex-col items-center py-30 gap-20 text-my-black w-full h-full">
+      <div className="px-4 flex flex-col gap-10 w-[85%] md:w-[50%]">
+        {/* TITLE */}
+        <motion.h1 
+          initial= {{ opacity: 0, y: 20 }}
+          animate= {{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="text-3xl md:text-4xl text-my-cyan border-my-cyan border-double border-l-8 pl-3"
+        >
+          {project.title}
+        </motion.h1>
         
-        <p className="leading-relaxed">
-          {project.desc}
-        </p>
+        {/* IMG & DESC */}
+        <motion.figure 
+          initial= {{ opacity: 0, y: 20 }}
+          animate= {{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.4 }}
+          viewport={{ once: true }}
+          className="flex flex-col gap-4"
+        >
+          <img src={project.imgSrc} className="w-full md:h-96 object-contain self-center"/>
+          <div className="flex justify-between items-center md:px-5 py-1 border-my-black/40 border-y italic text-xs md:text-base">
+            <p>
+              GitHub: {project.github ? <a href={project.github} target="_blank" className="font-semibold text-my-cyan">Link</a> : '[Private]'}
+            </p>
+            {project.link ? (
+              <a href={project.link} target="_blank" className="font-semibold text-my-red">
+                <p className='flex gap-2 items-center'><LinkIcon addClass="size-3 md:size-5" /> Open Live Site</p>
+              </a>
+            ) : (
+              <p className="flex gap-2 items-center text-my-red font-semibold"><UnlinkIcon addClass="size-3 md:size-5"/> No Live Site</p>
+            )}
+          </div>
+          
+          <motion.p 
+            initial= {{ opacity: 0, y: 20 }}
+            animate= {{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 0.5 }}
+            viewport={{ once: true }}
+            className="md:text-lg leading-relaxed md:pl-5"
+          >
+            {project.desc}
+          </motion.p>
+        </motion.figure>
 
-        <div className="flex flex-col gap-2">
+        {/* ROLE */}
+        <motion.div 
+          initial= {{ opacity: 0, y: 20 }}
+          animate= {{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          viewport={{ once: true }}
+          className="flex flex-col gap-5"
+        >
           <h2 className="text-2xl md:text-3xl font-semibold text-my-cyan border-my-cyan border-l-3 pl-3">My Role Here</h2>
-          <p>Fully built & maintain by me</p>
-        </div>
+          <motion.p 
+            initial= {{ opacity: 0, y: 20 }}
+            animate= {{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 0.7 }}
+            viewport={{ once: true }}
+            className="text-lg pl-5"><span className="text-my-red text-xl font-bold"
+          >
+            &#91;</span> Fully built & maintain by me <span className="text-my-red text-xl font-bold">&#93;</span>
+          </motion.p>
+        </motion.div>
 
-        <div className="flex flex-col gap-2 text-xs md:text-base text-my-black">
-          <h2 className="text-2xl md:text-3xl font-semibold text-my-cyan">Tech Stack</h2>
-          <div className="flex flex-wrap gap-2">
+        <motion.div 
+          initial= {{ opacity: 0, y: 20 }}
+          whileInView= {{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          viewport={{ once: true }}
+          className="flex flex-col gap-5 text-xs md:text-base text-my-black"
+        >
+          <h2 className="text-2xl md:text-3xl font-semibold text-my-cyan border-my-cyan border-l-3 pl-3">Tech Stack</h2>
+          <div className="flex flex-wrap gap-2 pl-5">
           {project.tags.map((tag, idx) => (
-              <TagCard
+              <motion.div
                 key={idx}
-                child={tag.child}
-                className="bg-my-cyan/50 text-my-black"
-              />
+                initial= {{ opacity: 0, y: 20 }}
+                animate= {{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.4, delay: 0.7 + idx * 0.1 }}
+                viewport={{ once: true }}
+              >
+                <TagCard
+                  key={idx}
+                  child={tag.child}
+                  className="bg-my-cyan/50 text-my-black"
+                />
+              </motion.div>
           ))}
           </div>
-        </div>
+        </motion.div>
 
-        <div className="flex flex-col gap-2">
-          <h2 className="text-2xl md:text-3xl font-semibold text-my-cyan">Feature</h2>
-          <ul className="list-disc list-inside">
+        <motion.div 
+          initial= {{ opacity: 0, y: 20 }}
+          whileInView= {{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.6 }}
+          viewport={{ once: true }}
+          className="flex flex-col gap-5"
+        >
+          <h2 className="text-2xl md:text-3xl font-semibold text-my-cyan border-my-cyan border-l-3 pl-3">Feature</h2>
+          <ul className="pl-5 font-semibold flex flex-col gap-1">
             {project.features && project.features.map((feature, idx) => (
-              <li key={idx}>{feature}</li>
+              <motion.div
+                key={idx}
+                initial= {{ opacity: 0, y: 20 }}
+                whileInView= {{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.4, delay: 0.7 + idx * 0.1 }}
+                viewport={{ once: true }}
+              >
+                <li key={idx}><p>{feature}</p></li>
+              </motion.div>
             ))}
           </ul>
-        </div>
+        </motion.div>
 
-        <div className="flex flex-col gap-2">
-          <h2 className="text-2xl md:text-3xl font-semibold text-my-cyan">Challenges & Solutions</h2>
-          <p className="italic leading-relaxed">
-            {project.challenges}
-          </p>
+        <div className="flex flex-col gap-5">
+          <motion.h2
+            initial= {{ opacity: 0, y: 20 }}
+            whileInView= {{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 0.6 }}
+            viewport={{ once: true }}
+            className="text-2xl md:text-3xl font-semibold text-my-cyan border-my-cyan border-l-3 pl-3"
+          >
+            Challenges & Solutions
+          </motion.h2>
+          <motion.p 
+            initial= {{ opacity: 0, y: 20 }}
+            whileInView= {{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 0.7 }}
+            viewport={{ once: true }}
+            className="italic leading-relaxed text-center"
+          >
+            "{project.challenges}"
+          </motion.p>
         </div>
 
       </div>
