8c2cb2b444e318d5b863a00c0ff42e8e7409427c
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2025-06-14 17:47:41 +0700

    change bg format

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Header activeSection={activeSection} onSectionChange={setActiveSection} />
-      <main className='flex justify-center items-center min-h-screen relative overflow-hidden bg-[url(/assets/pattern.jpg)] bg-[length:1200px_1200px]'>
+      <main className='flex justify-center items-center min-h-screen relative overflow-hidden bg-[url(/assets/pattern.webp)] bg-[length:1200px_1200px]'>
         <div className='bg-my-white/80 absolute inset-0 z-0 pointer-events-none backdrop-blur-lil' />
         <ScrollToTop>
           <Suspense fallback={<div>Loading...</div>}>
